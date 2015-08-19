cd94a0d55ac748e44e0673c191e90146fdb43dab
Sarah Llewelyn <sllewelyn@mdsol.com>
2015-08-18 20:53:13 -0400

    list displaying

--- todo.js
+++ todo.js
@@ -2,12 +2,11 @@ var App = React.createClass({
 
     getInitialState: function(){
         return {
-//            listItems: []
+            listItems: []
         }
     },
     
     addListItem: function(){
-//        listItem.push('')
     },
 
     render: function(){
@@ -17,10 +16,8 @@ var App = React.createClass({
                 <h1>My todo list app</h1>
 
                 // Enter text component
-//                <EnterListItemTextBox addListItem={this.addListItem} />
 
                 // List
-//                <List list={this.state.listItems} />
             </div>
         );   
     } //end render
@@ -28,52 +25,6 @@ var App = React.createClass({
 });
 
 
-// Expects props: addListItem()
-var EnterListItemTextBox = React.createClass({
-//    
-//    handleClick: function() {
-//        console.log("I am clicked");
-//    },
-//    
-//    render: function() {
-//        return(
-//            <div>
-//                <input type="text" />
-//                <input type="submit" onClick={this.handleClick}/>
-//            </div>
-//        )
-//    }
-});
 
-
-// Expects props: list
-var List = React.createClass({
-//    
-//    render: function() {
-//        return(
-//            <div>
-//                //foreach list item
-////            {this.props.list}.forEach(function(element, index, array) {
-////                <ListItem item={element} />
-////            })
-//            </div>
-//        )
-//    }
-});
-
-
-// Expects props: item
-var ListItem = React.createClass({
-//    
-//    render: function() {
-//        return(
-//            <div>
-//                //list item
-//                {this.props.item}
-//            </div>
-//        )
-//    }
-//    
-});
 
 React.render(<App />,  document.getElementById("content"));
